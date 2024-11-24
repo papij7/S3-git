02ad131aab40062623fa9ba4ddf99ef06748caf6
Fritz <chevryfritz@gmail.com>
2024-11-24 08:33:28 -0500

    Changed

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
 backend "s3" {
-  bucket = "week6-fc-bucket-terraform"
+  bucket = "none"
   key = "project1/terraform.tfstate"
   region = "us-east-1"
-  dynamodb_table = "statefile-lock"
+  dynamodb_table = "none"
     encrypt = true
 }
 }
